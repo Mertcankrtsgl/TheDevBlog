4e8a6b06147a9443ea446f71aeea34c9d1788903
Mertcan <mert_06_can@outlook.com>
2022-09-02 15:38:01 +0300

    Add project files.

---
+++ TheDevBlog.API/Data/TheDevBlogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheDevBlog.API.Models.Entities;
+
+namespace TheDevBlog.API.Data
+{
+    public class TheDevBlogDbContext : DbContext
+    {
+        public TheDevBlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
